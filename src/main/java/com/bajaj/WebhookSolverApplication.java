b9c78e86777955581f0a1adb003768b16989c6cf
Bareera Qureishi <127762940+Bareeraq@users.noreply.github.com>
2025-12-01 17:55:25 +0530

    modified:   pom.xml 	modified:   src/main/java/com/bajaj/WebhookSolverApplication.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/bajaj/WebhookSolverApplication.java
+++ src/main/java/com/bajaj/WebhookSolverApplication.java
@@ -2,16 +2,9 @@ package com.bajaj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication  // Remove the RestTemplate bean from here
 public class WebhookSolverApplication {
-    
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
     
     public static void main(String[] args) {
         SpringApplication.run(WebhookSolverApplication.class, args);
